70baa3d64b3fd167f79b6c6df34c37548d860267
alfsistemas <alfsistemas@gmail.com>
2022-05-27 12:16:31 -0300

    Arquitetura de Microsserviços do 0 com ASP.NET, .NET 6 e C#

---
+++ GeekShopping/GeekShopping/GeekShopping.ProductAPI/Config/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValuesObject;
+using GeekShopping.ProductAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductVO, Product>();
+                config.CreateMap<Product, ProductVO>();
+            });
+            return mappingConfig;
+        }
+    }
+}
